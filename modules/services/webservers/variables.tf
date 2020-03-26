b8d8f8d09a6fbaf18fb8936198e9d356f7fa3b97
Ki Hong <ki.hong@RM-MERASO-MAC.local>
2020-03-26 16:52:14 -0700

    initial commit - modules and dev

---
+++ modules/services/webservers/variables.tf
@@ -0,0 +1,17 @@
+variable "server_port" {
+  description = "The port the web server will be listening"
+  type        = number
+  default     = 8080
+}
+
+variable "elb_port" {
+  description = "The port the elb will be listening"
+  type        = number
+  default     = 80
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+}
+
